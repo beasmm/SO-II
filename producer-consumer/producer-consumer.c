157fa3ff6fe25ece84a539ebc58b17a6655b169d
Beatriz Mira Mendes.git <beatriz.mira.mendes@tecnico.ulisboa.pt>
2023-01-15 02:46:03 +0000

    producer-consumer correction

--- producer-consumer/producer-consumer.c
+++ producer-consumer/producer-consumer.c
@@ -53,11 +53,12 @@ int pcq_enqueue(pc_queue_t *queue, void *elem) {
     if (elem == NULL) {
         return -1;
     }
-    if (queue->pcq_current_size == queue->pcq_capacity) {
-        pthread_mutex_lock(&queue->pcq_pusher_condvar_lock);
+    pthread_mutex_lock(&queue->pcq_pusher_condvar_lock);
+    while (queue->pcq_current_size == queue->pcq_capacity) {
         pthread_cond_wait(&queue->pcq_pusher_condvar, &queue->pcq_pusher_condvar_lock);
-        pthread_mutex_unlock(&queue->pcq_pusher_condvar_lock);
     }
+    pthread_mutex_unlock(&queue->pcq_pusher_condvar_lock);
+
     pthread_mutex_lock(&queue->pcq_current_size_lock);
     queue->pcq_current_size++;
     pthread_mutex_unlock(&queue->pcq_current_size_lock);
@@ -76,11 +77,12 @@ int pcq_enqueue(pc_queue_t *queue, void *elem) {
 
 
 void *pcq_dequeue(pc_queue_t *queue) {
-    if (queue->pcq_current_size == 0) {
-        pthread_mutex_lock(&queue->pcq_popper_condvar_lock);
+    pthread_mutex_lock(&queue->pcq_popper_condvar_lock);
+    while (queue->pcq_current_size == 0) {
         pthread_cond_wait(&queue->pcq_popper_condvar, &queue->pcq_popper_condvar_lock);
-        pthread_mutex_unlock(&queue->pcq_popper_condvar_lock);
     }
+    pthread_mutex_unlock(&queue->pcq_popper_condvar_lock);
+    
     pthread_mutex_lock(&queue->pcq_current_size_lock);
     queue->pcq_current_size--;
     pthread_mutex_unlock(&queue->pcq_current_size_lock);
